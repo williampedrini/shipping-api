b781f6f9b22495f874c8d836f37b0b1dd67dbb58
William Custodio <william@pedrinicustodio.com>
2020-07-30 19:36:54 +0100

    feat: Addition of use case that handles product supplier's searches.
    This use case will be responsible for performing searches used as support for the use case responsible for creating the shipping. Initially it will only contain the main query responsible for ordering the product's suppliers for the best delivery option.

---
+++ src/main/java/com/moteefe/shipping/usecase/FindSupplierProduct.java
@@ -0,0 +1,49 @@
+package com.moteefe.shipping.usecase;
+
+import com.moteefe.shipping.domain.entity.Supplier;
+import com.moteefe.shipping.domain.entity.SupplierProduct;
+import com.moteefe.shipping.domain.port.SupplierProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Objects.nonNull;
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+@Component
+@RequiredArgsConstructor
+public final class FindSupplierProduct {
+
+    private final SupplierProductRepository supplierProductRepository;
+
+    /**
+     * Search for all suppliers that delivery a certain order item for a specific region.
+     *
+     * @param orderItemName The order item name.
+     * @param orderRegion   The order region to be delivered.
+     * @return A list containing all found products per supplier.
+     */
+    @NotNull
+    public List<SupplierProduct> findAllByOrderItemNameAndRegionSortedByBestDelivery(@NotNull final String orderItemName, @NotNull final String orderRegion) {
+        final var comparator = Comparator.comparing(
+                SupplierProduct::getDeliveryTimes, (deliveryTimes1, deliveryTimes2) -> {
+                    final var deliveryTime1 = deliveryTimes1.get(orderRegion);
+                    final var deliveryTime2 = deliveryTimes2.get(orderRegion);
+                    return deliveryTime1.compareTo(deliveryTime2);
+                })
+                .thenComparing(SupplierProduct::getInStock, Integer::compareTo)
+                .thenComparing(SupplierProduct::getSupplier, Comparator.comparing(Supplier::getName));
+        return supplierProductRepository
+                .findAllByProductName(orderItemName)
+                .stream()
+                .filter(supplierProduct -> {
+                    final var deliveryTimes = supplierProduct.getDeliveryTimes();
+                    return nonNull(deliveryTimes.get(orderRegion));
+                })
+                .sorted(comparator)
+                .collect(toUnmodifiableList());
+    }
+}
