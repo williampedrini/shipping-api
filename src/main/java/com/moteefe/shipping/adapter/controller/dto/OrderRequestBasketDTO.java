56d569b117f2b63527ca3376ef007c41bbd918b5
William Custodio <william@pedrinicustodio.com>
2020-08-02 15:23:26 +0100

    fix: Fix for bean validation that were not working.
    The API responsible for performing the validation was not working with the latest Spring Boot version. With that in mind, the hibernate validator API was added. There were some validations for nested objects that were not working as well.

--- src/main/java/com/moteefe/shipping/adapter/controller/dto/OrderRequestBasketDTO.java
+++ src/main/java/com/moteefe/shipping/adapter/controller/dto/OrderRequestBasketDTO.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.Collection;
 
@@ -11,6 +12,7 @@ import java.util.Collection;
 @ApiModel(description = "Represents a basket which holds the items part of an order.")
 public class OrderRequestBasketDTO {
 
+    @Valid
     @NotEmpty(message = "At least one item is mandatory to create an order.")
     @ApiModelProperty(position = 1, value = "The items part of the order.", required = true)
     private Collection<OrderRequestBasketItemDTO> items;
--- src/main/java/com/moteefe/shipping/adapter/controller/dto/OrderRequestBasketItemDTO.java
+++ src/main/java/com/moteefe/shipping/adapter/controller/dto/OrderRequestBasketItemDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @ApiModel(description = "Represents an item part of an order.")
@@ -15,6 +16,7 @@ public class OrderRequestBasketItemDTO {
     @ApiModelProperty(position = 1, value = "The name of the product associated to an item.", required = true, example = "black_mug")
     private String product;
 
+    @NotNull(message = "The number of products is mandatory.")
     @Min(message = "At least one product is mandatory.", value = 1)
     @ApiModelProperty(position = 2, value = "The amount of products.", required = true, example = "1")
     private Integer count;
